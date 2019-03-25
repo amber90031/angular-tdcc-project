ae3d250562fc96e1ee0ebacf912ab618061117eb
luna <luna608310@gmail.com>
2019-03-25 18:22:39 +0800

    分割index與meetings

--- src/app/index/index.module.ts
+++ src/app/index/index.module.ts
@@ -4,10 +4,22 @@ import { IndexComponent } from './index/index.component';
 import { MeetingsComponent } from './meetings/meetings.component';
 import { RouterModule } from '@angular/router';
 import { IndexRoutingModule } from './index-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [IndexComponent, MeetingsComponent],
-  imports: [CommonModule, RouterModule, IndexRoutingModule],
-  exports: [IndexComponent, MeetingsComponent]
+  declarations: [
+    IndexComponent,
+    MeetingsComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    IndexRoutingModule,
+    FormsModule
+  ],
+  exports: [
+    IndexComponent,
+    MeetingsComponent
+  ]
 })
 export class IndexModule {}
