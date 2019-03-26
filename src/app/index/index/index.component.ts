4eb8de60c6b192e2148ea7c2b9989e427a9534da
charley.chang <charley.chang@esunergy.com>
2019-03-26 10:46:47 +0800

    輪播公告

--- src/app/index/index/index.component.ts
+++ src/app/index/index/index.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, timer } from 'rxjs';
+import { take, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-index',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index.component.css']
 })
 export class IndexComponent implements OnInit {
+  // 公告
+  anmtStr: string;
+  // 公告切換秒數
+  anmtSecond: number;
+  // 公告內容
+  anmtList: string[] = ['aaa', 'bbb', 'ccc', 'ddd'];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.anmtSecond = 5;
+    console.log('constructor');
   }
 
+  ngOnInit() {
+    console.log('on init');
+    this.changeAnmtStr();
+  }
+
+  /**
+   * 修改最新公告
+   *
+   * @memberof IndexComponent
+   */
+  changeAnmtStr() {
+    timer(0, 1000 * this.anmtSecond).subscribe(tmr => {
+      const anmtLength = this.anmtList.length;
+      const anmtCnt = (tmr + anmtLength) % anmtLength;
+      this.anmtStr = this.anmtList[anmtCnt];
+    });
+  }
 }
